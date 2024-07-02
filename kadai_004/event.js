ba7ea29e2393adcd9a44cc370ef253088b6e3a5e
shota24kobashigawa <159881264+shota24kobashigawa@users.noreply.github.com>
2024-07-03 08:09:00 +0900

    Delete kadai_004 directory

--- kadai_004/event.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // HTMLドキュメントが読み込み完了したとき
-  $(window).on('load', function() {
-    console.log('loadイベントが発生しました');
-  });
-  $(window).on('scroll', () => {
-    console.log('scrollイベントが発生しました');
-  });
-});
